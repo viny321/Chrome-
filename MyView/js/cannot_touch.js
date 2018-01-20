ee8a7b4b762190f10efe7e44c878d89600a7bb6f
viny321 <1812436198@qq.com>
2018-01-20 15:43:30 +0800

    test git for vs code

---
+++ MyView/js/cannot_touch.js
@@ -0,0 +1,13 @@
+function btn_move(el,mouseleft,mousetop){
+	el.innerHTML = "a";
+}
+
+function over_btn(e){
+	if(!e){
+		e = window.event;
+	}
+	//btn_move(this,e.clientX,e.clientY);
+	this.innerHTML = "a";
+}
+
+document.getElementById('atext').onmouseover=over_btn;
