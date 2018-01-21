bbde73d164b0b665bed666cd4cd94eb136233d88
viny321 <1812436198@qq.com>
2018-01-21 10:58:27 +0800

    课表进行中...

--- WatchIP/js/my_ip.js
+++ WatchIP/js/my_ip.js
@@ -22,3 +22,46 @@ httpRequest('http://202.192.240.29/xsbjkbcx!xsAllKbList.action?xnxqdm=201701&bjd
 });
 
 //('/\[\{.*\}\]/')
+
+/**  注意：非最终版
+     * author:viny
+     * prams:oldarr 旧课表星期数组
+     * return: arr 转换出来的新的数组
+     */
+    function transforms(str){
+        var kcobj = JSON.parse(str);
+        var oldarr = kcobj.zcs.split(",");//对象提取字符串 转数组
+
+        oldarr.sort(function(a,b){return a-b});//数字升序
+        var zcs = "";
+        var arr = new Array;
+        for (let index = 0; index < oldarr.length; index++) {
+            if(index==0){//记录第一个数
+                var i = 0;
+                arr[i] = oldarr[0]+"";
+                var temp = oldarr[0];
+            }else if(index<oldarr.length-1){
+                zcs = oldarr[index];
+                if((zcs - temp)!=1){ //如果相邻两个数只差不为 1 ，记录周段，是接收数组加一 ，存储下一个周段
+                    if(!arr[i]==temp){//如果两数相等 则不需要 “~” 分隔
+                        arr[i] = arr[i]+"~"+temp;
+                    }
+                    i++;
+                    arr[i] = zcs+"";
+                }
+                temp = zcs;
+            }else{//记录最后一个数
+                zcs = oldarr[index];
+                if((zcs-temp)==1){
+                    arr[i] = arr[i]+"~"+zcs;
+                }else{
+                    arr[i] = arr[i]+"~"+temp;
+                    i++;
+                    arr[i] = zcs+"";
+                }
+            }
+            //console.log(oldarr[index]); //循环数组内容
+        }
+        var kcstr = kcobj.kcmc+"\n第"+arr.toString()+"周\n"+kcobj.jxcdmcs+"  "+kcobj.teaxms;
+        return kcstr;
+    }
